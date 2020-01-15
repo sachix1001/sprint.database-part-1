c5a5ada14f4979f4b518f20d1dab976e1b33e85f
Sarah and sachix1001 and GentaShibasaki <sarah.j.ting+sachix1001+genta.shibasaki.78@gmail.com>
2020-01-15 17:24:41 +0900

    write oldestInTownByGender.sql

--- scripts/selecting/oldestInTownByGender.sql
+++ scripts/selecting/oldestInTownByGender.sql
@@ -1,1 +1,7 @@
 -- Your SQL here
+select distinct students.* from students, 
+  (select min(date_of_birth) min_date_of_birth, town_of_origin from students where gender = 'F' GROUP BY town_of_origin) Oldestfemale,
+  (select min(date_of_birth) min_date_of_birth, town_of_origin from students where gender = 'M' GROUP BY town_of_origin) Oldestmale
+where
+  (students.gender = 'F' AND students.date_of_birth = Oldestfemale.min_date_of_birth AND students.town_of_origin = Oldestfemale.town_of_origin) or
+  (students.gender = 'M' AND students.date_of_birth = Oldestmale.min_date_of_birth AND students.town_of_origin = Oldestmale.town_of_origin);

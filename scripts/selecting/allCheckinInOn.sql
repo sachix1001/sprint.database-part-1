1a2d15a042d18497f628bcc8309266cb5a9deed3
Sarah and sachix1001 and GentaShibasaki <sarah.j.ting+sachix1001+genta.shibasaki.78@gmail.com>
2020-01-16 10:09:09 +0900

    basic requirement progress

--- scripts/selecting/allCheckinInOn.sql
+++ scripts/selecting/allCheckinInOn.sql
@@ -1,1 +1,3 @@
 --  your SQL here
+SELECT DISTINCT students.* FROM students inner join checkins on checkins.student_id = students.id 
+where date_part('year', checked_in_at) = 2016 AND date_part('month', checked_in_at) = 6;
--- scripts/selecting/allStudentAndCheckin.sql
+++ scripts/selecting/allStudentAndCheckin.sql
@@ -1,1 +1,2 @@
 -- Your SQL here
+SELECT * FROM students inner join checkins on checkins.student_id = students.id;
